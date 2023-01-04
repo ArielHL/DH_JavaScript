1205039140e9e2fefc74cbdcf617e56668520038
ArielHL <73503229+ArielHL@users.noreply.github.com>
2023-01-03 23:49:11 -0300

    UpDate Database

--- Project_ViandasApp/ViandasApp - v4/database/config/config.js
+++ Project_ViandasApp/ViandasApp - v4/database/config/config.js
@@ -1,11 +1,11 @@
 module.exports ={
   "development": {
     "username": "root",
-    "password": 'Sheldon3286',
+    "password": 'Lucas*3451/',
     "database": "HIMV",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": 8889,
+    "port": 3306,
   },
   "test": {
     "username": "root",
